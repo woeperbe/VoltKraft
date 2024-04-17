5b71399ac99d93a8621ba89bef5689702e8631ba
Marc Buffet <marc.buffet@woeper.be>
2024-04-17 12:18:31 +0200

    Add project files.

---
+++ MainForm.Designer.vb
@@ -0,0 +1,141 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        SendCommand = New Button()
+        CommandSend = New TextBox()
+        ReturnedVoltage = New TextBox()
+        ReadVoltKraftTimer = New Timer(components)
+        ReturnedAmpere = New TextBox()
+        ReturnedWatt = New TextBox()
+        PowerTypeModel = New TextBox()
+        PowerSwitch = New Button()
+        SuspendLayout()
+        ' 
+        ' SendCommand
+        ' 
+        SendCommand.Location = New Point(848, 547)
+        SendCommand.Name = "SendCommand"
+        SendCommand.Size = New Size(162, 86)
+        SendCommand.TabIndex = 0
+        SendCommand.Text = "SEND"
+        SendCommand.UseVisualStyleBackColor = True
+        ' 
+        ' CommandSend
+        ' 
+        CommandSend.Location = New Point(105, 188)
+        CommandSend.Name = "CommandSend"
+        CommandSend.Size = New Size(358, 31)
+        CommandSend.TabIndex = 1
+        ' 
+        ' ReturnedVoltage
+        ' 
+        ReturnedVoltage.BackColor = Color.Black
+        ReturnedVoltage.Font = New Font("Consolas", 28.0F, FontStyle.Bold, GraphicsUnit.Point)
+        ReturnedVoltage.ForeColor = Color.FromArgb(CByte(192), CByte(192), CByte(0))
+        ReturnedVoltage.Location = New Point(105, 295)
+        ReturnedVoltage.Name = "ReturnedVoltage"
+        ReturnedVoltage.ReadOnly = True
+        ReturnedVoltage.Size = New Size(300, 73)
+        ReturnedVoltage.TabIndex = 2
+        ReturnedVoltage.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' ReadVoltKraftTimer
+        ' 
+        ReadVoltKraftTimer.Enabled = True
+        ReadVoltKraftTimer.Interval = 500
+        ' 
+        ' ReturnedAmpere
+        ' 
+        ReturnedAmpere.BackColor = Color.Black
+        ReturnedAmpere.Font = New Font("Consolas", 28.0F, FontStyle.Bold, GraphicsUnit.Point)
+        ReturnedAmpere.ForeColor = Color.FromArgb(CByte(192), CByte(192), CByte(0))
+        ReturnedAmpere.Location = New Point(105, 374)
+        ReturnedAmpere.Name = "ReturnedAmpere"
+        ReturnedAmpere.Size = New Size(300, 73)
+        ReturnedAmpere.TabIndex = 3
+        ReturnedAmpere.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' ReturnedWatt
+        ' 
+        ReturnedWatt.BackColor = Color.Black
+        ReturnedWatt.Font = New Font("Consolas", 28.0F, FontStyle.Bold, GraphicsUnit.Point)
+        ReturnedWatt.ForeColor = Color.FromArgb(CByte(192), CByte(192), CByte(0))
+        ReturnedWatt.Location = New Point(105, 453)
+        ReturnedWatt.Name = "ReturnedWatt"
+        ReturnedWatt.Size = New Size(300, 73)
+        ReturnedWatt.TabIndex = 4
+        ReturnedWatt.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' PowerTypeModel
+        ' 
+        PowerTypeModel.Font = New Font("Consolas", 16.0F, FontStyle.Bold, GraphicsUnit.Point)
+        PowerTypeModel.ForeColor = Color.Red
+        PowerTypeModel.Location = New Point(82, 12)
+        PowerTypeModel.Name = "PowerTypeModel"
+        PowerTypeModel.Size = New Size(883, 45)
+        PowerTypeModel.TabIndex = 6
+        PowerTypeModel.Text = "BLABLA"
+        PowerTypeModel.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' PowerSwitch
+        ' 
+        PowerSwitch.BackColor = Color.Red
+        PowerSwitch.Font = New Font("Consolas", 24.0F, FontStyle.Bold, GraphicsUnit.Point)
+        PowerSwitch.ForeColor = Color.Black
+        PowerSwitch.Location = New Point(827, 198)
+        PowerSwitch.Name = "PowerSwitch"
+        PowerSwitch.Size = New Size(114, 94)
+        PowerSwitch.TabIndex = 7
+        PowerSwitch.Text = "OFF"
+        PowerSwitch.UseVisualStyleBackColor = False
+        ' 
+        ' MainForm
+        ' 
+        AutoScaleDimensions = New SizeF(10.0F, 25.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1040, 667)
+        Controls.Add(PowerSwitch)
+        Controls.Add(PowerTypeModel)
+        Controls.Add(ReturnedWatt)
+        Controls.Add(ReturnedAmpere)
+        Controls.Add(ReturnedVoltage)
+        Controls.Add(CommandSend)
+        Controls.Add(SendCommand)
+        Name = "MainForm"
+        Text = "VolKraft Power Controller"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents SendCommand As Button
+    Friend WithEvents CommandSend As TextBox
+    Friend WithEvents ReturnedVoltage As TextBox
+    Friend WithEvents ReadVoltKraftTimer As Timer
+    Friend WithEvents ReturnedAmpere As TextBox
+    Friend WithEvents ReturnedWatt As TextBox
+    Friend WithEvents PowerTypeModel As TextBox
+    Friend WithEvents PowerSwitch As Button
+
+End Class
---
+++ MainForm.vb
@@ -0,0 +1,130 @@
+﻿
+Imports System.IO.Ports
+
+
+Public Class MainForm
+
+    Dim WithEvents VoltKraftPort As New SerialPort
+    Dim receivedData As String
+    Dim voltage As String
+    Dim ampere As String
+    Dim watt As String
+    Dim v As Decimal
+    Dim a As Decimal
+    Dim w As Decimal
+    Dim PowerOn As Boolean
+
+
+
+
+    Private Sub MainForm(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            ' Set COM port settings
+            With VoltKraftPort
+                .PortName = "COM4"
+                .BaudRate = 115200
+                .Parity = Parity.None
+                .StopBits = StopBits.One
+                .DataBits = 8
+                .Handshake = Handshake.None
+                .Encoding = System.Text.Encoding.Default
+            End With
+            ' Open the serial port
+            VoltKraftPort.Open()
+        Catch ex As Exception
+            MessageBox.Show("Error: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+        ReadVoltKraftTimer.Stop()
+
+        VoltKraftPort.Write("*IDN?")
+        VoltKraftPort.Write(Chr(13))
+        receivedData = VoltKraftPort.ReadLine()
+        PowerTypeModel.Text = receivedData
+
+        VoltKraftPort.Write("OUTPUT?")
+        VoltKraftPort.Write(Chr(13))
+        receivedData = VoltKraftPort.ReadLine()
+        If (receivedData.Contains("1")) Then
+            PowerSwitch.Text = "ON"
+            PowerSwitch.BackColor = Color.Green
+            PowerOn = True
+        End If
+        If (receivedData.Contains("0")) Then
+            PowerSwitch.Text = "OFF"
+            PowerSwitch.BackColor = Color.Red
+            PowerOn = False
+        End If
+
+        ReadVoltKraftTimer.Start()
+
+    End Sub
+
+    Private Sub SendCommand_Click(sender As Object, e As EventArgs) Handles SendCommand.Click
+        Try
+            Dim message As String = CommandSend.Text
+            ' Send the message through the serial port
+            VoltKraftPort.Write(message)
+            VoltKraftPort.Write(Chr(13))
+        Catch ex As Exception
+            MessageBox.Show("Error: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub MainForm_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        ' Close the serial port when the form is closing
+        If VoltKraftPort.IsOpen Then
+            VoltKraftPort.Close()
+        End If
+    End Sub
+
+    Private Sub ReadVoltKraftTimer_Tick(sender As Object, e As EventArgs) Handles ReadVoltKraftTimer.Tick
+        Dim position As Integer
+        Dim previous As Integer
+        VoltKraftPort.Write("MEASURE:ALL?")
+        VoltKraftPort.Write(Chr(13))
+        receivedData = VoltKraftPort.ReadLine()
+        If (receivedData.Length() > 10) Then
+            position = receivedData.IndexOf(",")
+            voltage = receivedData.Substring(0, position)
+            previous = position
+            position = receivedData.IndexOf(",", previous + 1)
+            ampere = receivedData.Substring(previous + 1, position - previous - 1)
+            watt = receivedData.Substring(position + 1)
+            v = CDec(voltage) / 1000
+            a = CDec(ampere) / 1000
+            w = CDec(watt) / 1000
+            ReturnedVoltage.Text = v.ToString("F4") + " V"
+            ReturnedAmpere.Text = a.ToString("F4") + " A"
+            ReturnedWatt.Text = w.ToString("F4") + " W"
+        End If
+
+
+    End Sub
+
+    Private Sub PowerSwitch_Click(sender As Object, e As EventArgs) Handles PowerSwitch.Click
+        ReadVoltKraftTimer.Stop()
+        If (PowerOn = True) Then
+            PowerOn = False
+            VoltKraftPort.Write("OUTP 0")
+            VoltKraftPort.Write(Chr(13))
+            PowerSwitch.Text = "OFF"
+            PowerSwitch.BackColor = Color.Red
+            PowerSwitch.Refresh()
+            ReadVoltKraftTimer.Start()
+            Return
+        End If
+        If (PowerOn = False) Then
+            PowerOn = True
+            VoltKraftPort.Write("OUTP 1")
+            VoltKraftPort.Write(Chr(13))
+            PowerSwitch.Text = "ON"
+            PowerSwitch.BackColor = Color.Green
+            PowerSwitch.Refresh()
+            ReadVoltKraftTimer.Start()
+            Return
+        End If
+
+    End Sub
+End Class
+
+
